9170b5a3a35b653290373416eda5eb5122e6af78
sunny79979 <usman.maqsood194@gmail.com>
2016-10-01 01:58:00 -0700

    updated with log test file

--- first_test.go
+++ first_test.go
@@ -3,6 +3,8 @@ package main
 import (
         "testing"
         "strconv"
+        "log"
+        "os"
 )
 
 func TestURLS(t *testing.T){
@@ -14,10 +16,27 @@ func TestURLS(t *testing.T){
 
         for _, c := range cases {
         count := find(c.url, c.search)
+        var originalFile *os.File
         if count != c.expected {
-                t.Log("count should be " + strconv.Itoa(c.expected) + " but got$
+                _, err := os.Stat("test.txt")
+                if err != nil {
+                        originalFile, err = os.Create("test.txt")
+                        if err != nil {
+                                log.Fatal(err)
+                        }
+                }
+                originalFile, err = os.OpenFile("test.txt", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666,)
+                if err != nil {
+                        log.Fatal(err)
+                }
+                byt := []byte("count should be " + strconv.Itoa(c.expected) + " but got " + strconv.Itoa(count) + "\n")
+                _,err = originalFile.Write(byt)
+                if err!=nil{
+                        log.Fatal(err)
+                }
                 t.Fail()
         }
+        defer originalFile.Close()
         }
 
 }
